fc41d1237f1f51f1783d97ca2b33b339c08721e2
William Mantly <wmantly@gmail.com>
2021-01-14 22:03:15 -0500

    Update ldap-ssh-key.sh

--- files/ldap-ssh-key.sh
+++ files/ldap-ssh-key.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ldapsearch -h {{ldap_host}} -D "{{ldap_bind_dn}}" -w "{{ldap_bind_password}}" '(&(|(memberof=cn=host_access,ou=groups,dc=theta42,dc=com)(memberof=cn=host_{{current_host}}_access,ou=groups,dc=theta42,dc=com))(uid='"$1"'))' 'sshPublicKey' | sed -n '/^ /{H;d};/sshPublicKey:/x;$g;s/\n *//g;s/sshPublicKey: //gp'
+ldapsearch -h {{ldap_host}} -D "{{ldap_bind_dn}}" -w "{{ldap_bind_password}}" '(&(|(memberof=cn=host_access,ou=groups,dc=dataconinfra,dc=net)(memberof=cn=host_{{current_host}}_access,ou=groups,dc=dataconinfra,dc=net))(uid='"$1"'))' 'sshPublicKey' | sed -n '/^ /{H;d};/sshPublicKey:/x;$g;s/\n *//g;s/sshPublicKey: //gp'
